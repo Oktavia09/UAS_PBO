01cb145b838cc6f1c229df1cce19192773b4da89
Oktavia09 <okatavii@gmail.com>
2024-12-11 21:58:27 +0700

    hallo

---
+++ login3.py
@@ -0,0 +1,320 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import mysql.connector
+from mysql.connector import Error
+import bcrypt
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+import mysql.connector
+from mysql.connector import Error
+
+class Ui_MainWindow(object):
+    def __init__(self):
+        self.connection = None
+        self.cursor = None
+        self.create_connection()
+
+    def create_connection(self):
+        """Create a database connection to a MySQL database."""
+        try:
+            self.connection = mysql.connector.connect(
+                host='localhost',
+                user='root',  # Replace with your MySQL username
+                password='',  # Replace with your MySQL password
+                database='aplikasi_pengelolaan_donasi'  # Database name
+            )
+            if self.connection.is_connected():
+                self.cursor = self.connection.cursor()
+                print("Connection to database successful")
+        except Error as e:
+            print(f"Error connecting to MySQL database: {e}")
+   
+    def check_login(self, username, password):
+        """Check login credentials against the database."""
+        query = "SELECT password FROM users WHERE username = %s OR email = %s"
+        self.cursor.execute(query, (username, username))
+        result = self.cursor.fetchone()
+        # Directly compare plaintext passwords
+        if result and result[0] == password:
+            return True
+        return False
+    
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(802, 700)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+
+        # Create Frame for UI
+        self.frame = QtWidgets.QFrame(self.centralwidget)
+        self.frame.setGeometry(QtCore.QRect(0, 0, 881, 671))
+        self.frame.setStyleSheet("background-color:#CC95C0;")
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        
+        self.frame_5 = QtWidgets.QFrame(self.frame)
+        self.frame_5.setGeometry(QtCore.QRect(80, 20, 631, 121))
+        self.frame_5.setStyleSheet("background-color:#DBD4B4;\nborder-radius:20px;")
+        self.frame_5.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame_5.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame_5.setObjectName("frame_5")
+        
+        self.label_3 = QtWidgets.QLabel(self.frame_5)
+        self.label_3.setGeometry(QtCore.QRect(230, 20, 271, 41))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_3.setFont(font)
+        self.label_3.setObjectName("label_3")
+        
+        self.label_4 = QtWidgets.QLabel(self.frame_5)
+        self.label_4.setGeometry(QtCore.QRect(160, 70, 401, 31))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        self.label_4.setFont(font)
+        self.label_4.setObjectName("label_4")
+        
+        self.label_5 = QtWidgets.QLabel(self.frame_5)
+        self.label_5.setGeometry(QtCore.QRect(10, 0, 141, 121))
+        self.label_5.setText("")
+        self.label_5.setPixmap(QtGui.QPixmap("../../Downloads/bg-transparent-bank.png"))
+        self.label_5.setScaledContents(True)
+        self.label_5.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignTop)
+        self.label_5.setObjectName("label_5")
+        
+        self.label = QtWidgets.QLabel(self.frame)
+        self.label.setGeometry(QtCore.QRect(10, 243, 261, 101))
+        font = QtGui.QFont()
+        font.setPointSize(21)
+        self.label.setFont(font)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+
+        self.label_2 = QtWidgets.QLabel(self.frame)
+        self.label_2.setGeometry(QtCore.QRect(10, 350, 261, 101))
+        font = QtGui.QFont()
+        font.setPointSize(21)
+        self.label_2.setFont(font)
+        self.label_2.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_2.setObjectName("label_2")
+        
+        self.lineEdit_3 = QtWidgets.QLineEdit(self.frame)
+        self.lineEdit_3.setGeometry(QtCore.QRect(250, 280, 461, 40))
+        self.lineEdit_3.setStyleSheet("background-color:#DBD4B4;\nborder-radius:5px;\nfont-size:20px;")
+        self.lineEdit_3.setAlignment(QtCore.Qt.AlignCenter)
+        self.lineEdit_3.setObjectName("lineEdit_3")
+        
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.frame)
+        self.lineEdit_2.setGeometry(QtCore.QRect(250, 380, 461, 41))
+        self.lineEdit_2.setStyleSheet("background-color:#DBD4B4;\nborder-radius:5px;\nfont-size:20px;")
+        self.lineEdit_2.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.lineEdit_2.setAlignment(QtCore.Qt.AlignCenter)
+        self.lineEdit_2.setObjectName("lineEdit_2")
+
+        self.pushButton = QtWidgets.QPushButton(self.frame)
+        self.pushButton.setGeometry(QtCore.QRect(301, 532, 181, 41))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton.setFont(font)
+        self.pushButton.setStyleSheet("background-color:#DBD4B4;\nborder-radius:10px;")
+        self.pushButton.setObjectName("pushButton")
+        
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        # Menubar and statusbar
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 802, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+        # Connect the login button to the login handler
+        self.pushButton.clicked.connect(self.handle_login)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label_3.setText(_translate("MainWindow", "Selamat Datang"))
+        self.label_4.setText(_translate("MainWindow", "Rumah Kita Peduli Bersama"))
+        self.label.setText(_translate("MainWindow", "Username"))
+        self.label_2.setText(_translate("MainWindow", "Password"))
+        self.lineEdit_2.setPlaceholderText(_translate("MainWindow", "Masukkan password"))
+        self.pushButton.setText(_translate("MainWindow", "LOGIN"))
+        self.lineEdit_3.setPlaceholderText(_translate("MainWindow", "Masukkan username"))
+
+    def handle_login(self):
+        """Handle the login process."""
+        username = self.lineEdit_3.text()
+        password = self.lineEdit_2.text()
+        
+        # Check if the login credentials are correct
+        user = self.check_login(username, password)
+        
+        if user:
+            # If login is successful, show a message or perform an action
+            QtWidgets.QMessageBox.information(None, "Login Success", "Login berhasil!")
+            print("Login successful")
+        else:
+            # If login fails, show an error message
+            QtWidgets.QMessageBox.warning(None, "Login Failed", "Username atau password salah.")
+            print("Login failed")
+
+
+            
+    def show_new_window(self):
+        self.new_window = QtWidgets.QMainWindow()
+        self.ui = UI_form_donatur()
+        self.ui.setupUi(self.new_window)
+        self.new_window.show()
+
+class UI_form_donatur(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(800, 700)
+        Form.setMinimumSize(QtCore.QSize(800, 700))
+        Form.setMaximumSize(QtCore.QSize(800, 700))
+        Form.setStyleSheet("background-color:#CC95C0;")
+        
+        self.frame = QtWidgets.QFrame(Form)
+        self.frame.setGeometry(QtCore.QRect(0, 0, 908, 671))
+        self.frame.setStyleSheet("background-color:#CC95C0;")
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setObjectName("frame")
+        
+        self.frame_5 = QtWidgets.QFrame(self.frame)
+        self.frame_5.setGeometry(QtCore.QRect(80, 20, 631, 121))
+        self.frame_5.setStyleSheet("background-color:#DBD4B4;\n"
+                                   "border-radius:20px;")
+        self.frame_5.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame_5.setObjectName("frame_5")
+        
+        self.label_3 = QtWidgets.QLabel(self.frame_5)
+        self.label_3.setGeometry(QtCore.QRect(230, 20, 271, 41))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        font.setBold(True)
+        self.label_3.setFont(font)
+        self.label_3.setObjectName("label_3")
+        
+        self.label_4 = QtWidgets.QLabel(self.frame_5)
+        self.label_4.setGeometry(QtCore.QRect(160, 70, 401, 31))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        self.label_4.setFont(font)
+        self.label_4.setObjectName("label_4")
+        
+        self.label_5 = QtWidgets.QLabel(self.frame_5)
+        self.label_5.setGeometry(QtCore.QRect(10, 0, 141, 121))
+        self.label_5.setPixmap(QtGui.QPixmap("../../Downloads/bg-transparent-bank.png"))
+        self.label_5.setScaledContents(True)
+        self.label_5.setObjectName("label_5")
+        
+        self.label = QtWidgets.QLabel(self.frame)
+        self.label.setGeometry(QtCore.QRect(94, 222, 191, 81))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.label.setFont(font)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        
+        self.lineEdit_3 = QtWidgets.QLineEdit(self.frame)
+        self.lineEdit_3.setGeometry(QtCore.QRect(371, 245, 241, 40))
+        self.lineEdit_3.setStyleSheet("background-color:#DBD4B4;\n"
+                                      "border-radius:5px;\n"
+                                      "font-size:20px;")
+        self.lineEdit_3.setAlignment(QtCore.Qt.AlignCenter)
+        self.lineEdit_3.setObjectName("lineEdit_3")
+        
+        self.label_2 = QtWidgets.QLabel(self.frame)
+        self.label_2.setGeometry(QtCore.QRect(75, 287, 221, 51))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.label_2.setFont(font)
+        self.label_2.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_2.setObjectName("label_2")
+        
+        self.lineEdit_4 = QtWidgets.QLineEdit(self.frame)
+        self.lineEdit_4.setGeometry(QtCore.QRect(369, 298, 241, 40))
+        self.lineEdit_4.setStyleSheet("background-color:#DBD4B4;\n"
+                                      "border-radius:5px;\n"
+                                      "font-size:20px;")
+        self.lineEdit_4.setAlignment(QtCore.Qt.AlignCenter)
+        self.lineEdit_4.setObjectName("lineEdit_4")
+        
+        self.label_6 = QtWidgets.QLabel(self.frame)
+        self.label_6.setGeometry(QtCore.QRect(95, 343, 221, 41))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.label_6.setFont(font)
+        self.label_6.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_6.setObjectName("label_6")
+        
+        self.radioButton = QtWidgets.QRadioButton(self.frame)
+        self.radioButton.setGeometry(QtCore.QRect(370, 359, 131, 21))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.radioButton.setFont(font)
+        self.radioButton.setObjectName("radioButton")
+        
+        self.radioButton_2 = QtWidgets.QRadioButton(self.frame)
+        self.radioButton_2.setGeometry(QtCore.QRect(475, 350, 151, 41))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.radioButton_2.setFont(font)
+        self.radioButton_2.setObjectName("radioButton_2")
+        
+        self.pushButton = QtWidgets.QPushButton(self.frame)
+        self.pushButton.setGeometry(QtCore.QRect(272, 499, 241, 51))
+        self.pushButton.setStyleSheet("background-color:#DBD4B4;\n"
+                                      "border-radius:5px;\n"
+                                      "font-size:20px;")
+        self.pushButton.setObjectName("pushButton")
+        
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+    
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form Donatur"))
+        self.label_3.setText(_translate("Form", "Selamat Datang"))
+        self.label_4.setText(_translate("Form", "Rumah Kita Peduli Bersama"))
+        self.label.setText(_translate("Form", "Nama"))
+        self.lineEdit_3.setPlaceholderText(_translate("Form", "masukkan username"))
+        self.label_2.setText(_translate("Form", "email"))
+        self.lineEdit_4.setPlaceholderText(_translate("Form", "masukkan username"))
+        self.label_6.setText(_translate("Form", "Kategori"))
+        self.radioButton.setText(_translate("Form", "Individu"))
+        self.radioButton_2.setText(_translate("Form", "Organisasi"))
+        self.pushButton.setText(_translate("Form", "Simpan"))
+
+        
+if __name__ == "__main__":
+    import sys
+    
+    app = QtWidgets.QApplication(sys.argv)
+    
+    # Buat jendela utama
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    
+    # Buat formulir tambahan (QWidget)
+    Form = QtWidgets.QWidget()
+    ui_form_donatur = UI_form_donatur()
+    ui_form_donatur.setupUi(Form)
+    Form.show()  # Hanya panggil show(), jangan gunakan setCentralWidget()
+    
+    
+    # Jalankan aplikasi
+
+    sys.exit(app.exec_())
